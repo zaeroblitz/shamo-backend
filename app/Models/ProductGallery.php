ac51d1d80a78841626edcd3f3217cc3e6538751b
Zaero Blitz <zaeroblitz@gmail.com>
2021-06-30 16:46:21 +0700

    Add ProductGallery

---
+++ app/Models/ProductGallery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+
+class ProductGallery extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'products_id',
+        'url'
+    ];
+
+    // Get full image URL for API that can be consumed in Flutter
+    public function getAllUrlAttribute($url)
+    {
+        return config('app.url') . Storage::url($url);
+    }
+}
