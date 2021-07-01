970367bf530a5bd9fd64bb24d194419a9f131428
Zaero Blitz <zaeroblitz@gmail.com>
2021-07-01 17:51:13 +0700

    Add Register API

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Rules\Password;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'username' => ['required', 'string', 'max:255', 'unique:users'],
+                'email' => ['required', 'string', 'max:255', 'unique:users', 'email'],
+                'phone_number' => ['required', 'string', 'max:255'],
+                'password' => ['required', 'string', new Password]
+            ]);
+    
+            User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'phone_number' => $request->phone_number,
+                'password' => $request->password,
+            ]);
+    
+            $user = User::where('email', $request->email)->first();
+    
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+    
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'User Registered');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+}
