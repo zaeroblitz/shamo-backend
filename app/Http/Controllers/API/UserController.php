4fa20dadb81b5c6dc2486f28dc83b250a938fbe7
Zaero Blitz <zaeroblitz@gmail.com>
2021-07-02 08:58:04 +0700

    Add Login API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller
@@ -18,7 +20,7 @@ class UserController extends Controller
                 'name' => ['required', 'string', 'max:255'],
                 'username' => ['required', 'string', 'max:255', 'unique:users'],
                 'email' => ['required', 'string', 'max:255', 'unique:users', 'email'],
-                'phone_number' => ['required', 'string', 'max:255'],
+                'phone_number' => ['nullable', 'string', 'max:255'],
                 'password' => ['required', 'string', new Password]
             ]);
     
@@ -27,7 +29,7 @@ class UserController extends Controller
                 'username' => $request->username,
                 'email' => $request->email,
                 'phone_number' => $request->phone_number,
-                'password' => $request->password,
+                'password' => Hash::make($request->password),
             ]);
     
             $user = User::where('email', $request->email)->first();
@@ -39,6 +41,43 @@ class UserController extends Controller
                 'token_type' => 'Bearer',
                 'user' => $user
             ], 'User Registered');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)){
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Authenticated');
+
         } catch (Exception $error) {
             return ResponseFormatter::error([
                 'message' => 'Something went wrong',
--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,5 @@ Route::get('products', [ProductController::class, 'all']);
 
 Route::get('categories', [ProductCategoryController::class, 'all']);
 
-Route::get('register', [UserController::class, 'register']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
